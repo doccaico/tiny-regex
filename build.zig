1395f6486285b80a031d5be53d9729532b327cbd
doccaico <hash_ext_luna@yahoo.co.jp>
2025-07-21 15:33:32 +0900

    up

--- build.zig
+++ build.zig
@@ -14,10 +14,10 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    _ = b.addModule("libtiny-regex", .{
-        .root_source_file = upstream.path("re.c"),
-        .link_libc = true,
-    });
+    // _ = b.addModule("libtiny-regex", .{
+    //     .root_source_file = upstream.path("re.c"),
+    //     .link_libc = true,
+    // });
 
     lib.addCSourceFile(.{
         .file = upstream.path("re.c"),
